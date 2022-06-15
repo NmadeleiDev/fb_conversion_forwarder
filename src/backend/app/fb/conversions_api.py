a791a991b12f3688f5eeb8c893fdfa8b173f2d09
Gregory Potemkin <gregory@gomobile.ru>
2022-06-15 15:46:19 +0300

    d

--- src/backend/app/fb/conversions_api.py
+++ src/backend/app/fb/conversions_api.py
@@ -18,34 +18,42 @@ def lower_and_hash(val: str) -> str:
     return sha256(val.lower().encode('utf-8')).hexdigest()
 
 def create_fb_conversion_event_data(conversion: SendConversionRequest, ip: str, user_agent: str, event_source: str) -> dict:
-    return {
+    user_data = {
+        "em": [lower_and_hash(x) for x in conversion.emails] + conversion.hash_emails,
+        "ph": [lower_and_hash(filter_str_for_subset(x, string.digits).lstrip('0')) for x in conversion.phones] + conversion.hash_phones,
+        "fbc": conversion.fbc,
+        "fbp": conversion.fbp,
+        "ge": [x.value for x in conversion.genders],
+        "db": [lower_and_hash(x) for x in conversion.dates_of_birth] + conversion.hash_dates_of_birth,
+        "ln": [lower_and_hash(x) for x in conversion.last_names] + conversion.hash_last_names,
+        "ct": [lower_and_hash(x) for x in conversion.cities] + conversion.hash_cities,
+        "fn": [lower_and_hash(x) for x in conversion.first_names] + conversion.hash_first_names,
+        "country": [lower_and_hash(x) for x in conversion.countries] + conversion.hash_countries,
+    }
+    if ip:
+        user_data["client_ip_address"] = ip
+    if user_agent:
+        user_data["client_user_agent"] = user_agent
+    if conversion.lead_id:
+        user_data["lead_id"] = conversion.lead_id
+
+    req_data = {
                 "event_name": conversion.event_name,
                 "event_time": conversion.event_time,
-                "event_source_url": event_source,         
                 "action_source": "website",
-                "user_data": {
-                    "client_ip_address": ip,
-                    "client_user_agent": user_agent,
-                    "em": [lower_and_hash(x) for x in conversion.emails] + conversion.hash_emails,
-                    "ph": [lower_and_hash(filter_str_for_subset(x, string.digits).lstrip('0')) for x in conversion.phones] + conversion.hash_phones,
-                    "fbc": conversion.fbc,
-                    "fbp": conversion.fbp,
-                    "ge": [x.value for x in conversion.genders],
-                    "db": [lower_and_hash(x) for x in conversion.dates_of_birth] + conversion.hash_dates_of_birth,
-                    "ln": [lower_and_hash(x) for x in conversion.last_names] + conversion.hash_last_names,
-                    "ct": [lower_and_hash(x) for x in conversion.cities] + conversion.hash_cities,
-                    "fn": [lower_and_hash(x) for x in conversion.first_names] + conversion.hash_first_names,
-                    "country": [lower_and_hash(x) for x in conversion.countries] + conversion.hash_countries,
-                    "lead_id": conversion.lead_id,
-                },
+                "user_data": user_data,
             }
+    if event_source:
+        req_data["event_source_url"] = event_source
+
+    return req_data
 
 def send_convesrion(conversion: SendConversionRequest, ip: str, user_agent: str, event_source: str, 
                     pixel_id: str, access_token: str):
     PIXEL_ID = pixel_id
     TOKEN = access_token
 
-    logging.debug(f'Sending conversion to fb: {conversion.dict()}')
+    logging.debug(f'Sending conversion to fb with pixel_id={pixel_id}, access_token={access_token}: {conversion.dict()}')
     logging.debug(f'ip={ip}, user_agent={user_agent}, event_source={event_source}')
 
     resp = requests.post(
@@ -55,7 +63,7 @@ def send_convesrion(conversion: SendConversionRequest, ip: str, user_agent: str,
                 create_fb_conversion_event_data(conversion, ip, user_agent, event_source)
             ]
         })
-    logging.debug(f"Got resp form FB: {resp.text}")
+    logging.debug(f"Got resp form FB: {resp.json()}")
 
 
 def send_test_conversion(test_code: str, ip: str, user_agent: str, event_source: str, pixel_id: str, access_token: str):
