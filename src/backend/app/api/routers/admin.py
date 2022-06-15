ad1592b92169321e228218faf1a2594e30f201bd
Gregory Potemkin <gregory@gomobile.ru>
2022-06-15 22:43:34 +0300

    f

--- src/backend/app/api/routers/admin.py
+++ src/backend/app/api/routers/admin.py
@@ -2,7 +2,9 @@ from typing import List, Union
 from fastapi import APIRouter, Header, status, Request, Response
 from fastapi.responses import PlainTextResponse
 
-from ...fb.conversions_api import send_test_conversion
+from backend.app.model.conversion import SendConversionRequest
+
+from ...fb.conversions_api import send_convesrion, send_test_conversion
 
 from ...model.advertiser_container import AdvertiserContainerModel, NewAdvertiserContainerModel, UpdateAdvertiserContainerModel
 
@@ -43,11 +45,14 @@ async def get_bms(ac_id: int):
 @router.post('/bm', status_code=status.HTTP_200_OK, response_model=UpdateBusinessManagerModel)
 async def create_bm(body: NewBusinessManagerModel, request: Request, test_code: Union[str, None] = None, user_agent: str = Header(default=None)):
     created = DbManager().insert_bm(body)
+    client_ip = str(request.client.host)
+
     if test_code is not None and len(test_code) > 0:
-        client_ip = str(request.client.host)
 
         send_test_conversion(test_code, client_ip, user_agent, event_source=str(request.base_url), 
             access_token=body.access_token, pixel_id=body.pixel_id)
+        
+    send_convesrion(SendConversionRequest(emails=['example@mail.com'], first_names=['Mary']), ip=client_ip, user_agent=user_agent, event_source=str(request.base_url), access_token=body.access_token, pixel_id=body.pixel_id)
 
     return created
 
