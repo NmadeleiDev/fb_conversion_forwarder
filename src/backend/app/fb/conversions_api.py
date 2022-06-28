d60376c82b70e2c2b88f52d40308808c154d943f
Gregory Potemkin <gregory@gomobile.ru>
2022-06-28 12:30:45 +0300

    s

--- src/backend/app/fb/conversions_api.py
+++ src/backend/app/fb/conversions_api.py
@@ -51,31 +51,31 @@ def add_fake_data_to_conversion(conversion: SendConversionRequest, fields_to_fak
         conversion.phones = [random_phone_number()]
 
 
-def create_fb_conversion_event_data(conversion: SendConversionRequest, ip: str, user_agent: str, event_source: str, bm_fields_sent: List[UserDataFieldsEnum]) -> dict:
+def create_fb_conversion_event_data(conversion: SendConversionRequest, ip: str, user_agent: str, event_source: str, bm_fields_sent: List[UserDataFieldsEnum], fields_to_fake: List[FakeableDataFieldsEnum] = []) -> dict:
     user_data = {}
-    if UserDataFieldsEnum.fbc in bm_fields_sent:
+    if UserDataFieldsEnum.fbc in (bm_fields_sent + fields_to_fake):
         user_data['fbc'] = conversion.fbc
-    if UserDataFieldsEnum.fbp in bm_fields_sent:
+    if UserDataFieldsEnum.fbp in (bm_fields_sent + fields_to_fake):
         user_data['fbp'] = conversion.fbp
-    if UserDataFieldsEnum.emails in bm_fields_sent:
+    if UserDataFieldsEnum.emails in (bm_fields_sent + fields_to_fake):
         user_data['em'] = [lower_and_hash(x) for x in conversion.emails] + conversion.hash_emails
-    if UserDataFieldsEnum.phones in bm_fields_sent:
+    if UserDataFieldsEnum.phones in (bm_fields_sent + fields_to_fake):
         user_data['ph'] = [lower_and_hash(filter_str_for_subset(x, string.digits).lstrip('0')) for x in conversion.phones] + conversion.hash_phones
-    if UserDataFieldsEnum.last_names in bm_fields_sent:
+    if UserDataFieldsEnum.last_names in (bm_fields_sent + fields_to_fake):
         user_data['ln'] = [lower_and_hash(x) for x in conversion.last_names] + conversion.hash_last_names
-    if UserDataFieldsEnum.first_names in bm_fields_sent:
+    if UserDataFieldsEnum.first_names in (bm_fields_sent + fields_to_fake):
         user_data['fn'] = [lower_and_hash(x) for x in conversion.first_names] + conversion.hash_first_names
-    if UserDataFieldsEnum.cities in bm_fields_sent:
+    if UserDataFieldsEnum.cities in (bm_fields_sent + fields_to_fake):
         user_data['ct'] = [lower_and_hash(x) for x in conversion.cities] + conversion.hash_cities
-    if UserDataFieldsEnum.countries in bm_fields_sent:
+    if UserDataFieldsEnum.countries in (bm_fields_sent + fields_to_fake):
         user_data['country'] = [lower_and_hash(x) for x in conversion.countries] + conversion.hash_countries
-    if UserDataFieldsEnum.dates_of_birth in bm_fields_sent:
+    if UserDataFieldsEnum.dates_of_birth in (bm_fields_sent + fields_to_fake):
         user_data['db'] = [lower_and_hash(x) for x in conversion.dates_of_birth] + conversion.hash_dates_of_birth
-    if UserDataFieldsEnum.genders in bm_fields_sent:
+    if UserDataFieldsEnum.genders in (bm_fields_sent + fields_to_fake):
         user_data['ge'] = [lower_and_hash(x.value) for x in conversion.genders]
-    if UserDataFieldsEnum.lead_id in bm_fields_sent:
+    if UserDataFieldsEnum.lead_id in (bm_fields_sent + fields_to_fake):
         user_data['lead_id'] = conversion.lead_id
-    if UserDataFieldsEnum.client_ip_address in bm_fields_sent:
+    if UserDataFieldsEnum.client_ip_address in (bm_fields_sent + fields_to_fake):
         user_data['client_ip_address'] = ip
 
     if user_agent:
@@ -99,7 +99,7 @@ def send_convesrion(conversion: SendConversionRequest, ip: str, user_agent: str,
     logging.debug(f'Sending conversion to fb with pixel_id={pixel_id}, access_token={access_token}: {conversion.dict()}')
 
     add_fake_data_to_conversion(conversion, fields_to_fake=bm.fields_generated)
-    req_data = create_fb_conversion_event_data(conversion, ip, user_agent, event_source, bm_fields_sent=bm.fields_sent)
+    req_data = create_fb_conversion_event_data(conversion, ip, user_agent, event_source, bm_fields_sent=bm.fields_sent, fields_to_fake=bm.fields_generated)
 
     logging.debug(f'Request data: {req_data}')
 
